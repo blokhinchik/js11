7f9167bd6be11b028f5b859653c935096f4d5465
Dmytro Blokhin <blokhin98.d@gmail.com>
2023-04-27 17:51:53 +0300

    foop without password verify

--- main.js
+++ main.js
@@ -38,63 +38,164 @@
 
 // PASSWORD !!!
 
-// function Password(parent, open) {
-//   const input = document.createElement("input");
-//   input.type = "password";
-//   parent.append(input);
+function Password(parent, open) {
+  const input = document.createElement("input");
+  input.type = "password";
+  input.placeholder = "PASSWORD";
+  parent.append(input);
 
-//   const toggle = document.createElement("button");
-//   toggle.innerText = "Open";
-//   parent.append(toggle);
+  const toggle = document.createElement("button");
+  toggle.id = "toggle";
+  toggle.innerText = "Open";
+  parent.append(toggle);
 
-//   value = "";
-//   isOpen = open;
+  value = "";
+  isOpen = open;
 
-//   this.setValue = (newValue) => {
-//     value = newValue;
-//     input.value = value;
-//   };
-//   this.getValue = () => {
-//     return input.value;
-//   };
-//   this.setOpen = (newOpen) => {
-//     isOpen = newOpen;
-//     if (isOpen) {
-//       toggle.innerText = "Open";
-//       input.type = "text";
-//     } else {
-//       toggle.innerText = "Hide";
-//       input.type = "password";
-//     }
-//     this.getOpen = () => {
-//       return isOpen;
-//     };
-//   };
-//   input.oninput = () => {
-//     value = input.value;
-//     this.onChange(value);
-//   };
-//   toggle.onclick = () => {
-//     isOpen = !isOpen;
-//     if (isOpen) {
-//       toggle.innerText = "Open";
-//       input.type = "text";
-//     } else {
-//       toggle.innerText = "Hide";
-//       input.type = "password";
-//     }
-//     this.onOpenChange(isOpen);
-//   };
+  this.setValue = (newValue) => {
+    value = newValue;
+    input.value = value;
+  };
+  this.getValue = () => {
+    return input.value;
+  };
+  this.setOpen = (newOpen) => {
+    isOpen = newOpen;
+    if (isOpen) {
+      toggle.innerText = "Open";
+      input.type = "text";
+    } else {
+      toggle.innerText = "Hide";
+      input.type = "password";
+    }
+    this.getOpen = () => {
+      return isOpen;
+    };
+  };
+  input.oninput = () => {
+    value = input.value;
+    if (typeof this.onChange === "function") {
+      this.onChange(value);
+    }
+  };
+  toggle.onclick = () => {
+    isOpen = !isOpen;
+    if (isOpen) {
+      toggle.innerText = "Open";
+      input.type = "text";
+    } else {
+      toggle.innerText = "Hide";
+      input.type = "password";
+    }
+    if (typeof this.onOpenChange === "function") {
+      this.onOpenChange(isOpen);
+    }
+  };
+}
+
+// const passwordInput = new Password(document.body, false);
+
+// passwordInput.onChange = (data) => console.log(data); //буде корисно при виконаннi LoginForm та Password Verify
+// passwordInput.onOpenChange = (open) => console.log(open);
+
+// passwordInput.setValue("qwerty");
+// console.log(passwordInput.getValue());
+
+// passwordInput.setOpen(false);
+// console.log(passwordInput.getOpen());
+
+// LOGIN FORM !!!
+
+// const container = document.createElement("div");
+// document.body.append(container);
+
+// const loginInput = document.createElement("input");
+// loginInput.type = "text";
+// loginInput.placeholder = "LOGIN";
+// loginInput.oninput = () => {
+//   disabledSubmit();
+// };
+// container.append(loginInput);
+
+// const passwordInput = new Password(container, true);
+// passwordInput.onChange = () => {
+//   disabledSubmit();
+// };
+
+// const submitButton = document.createElement("button");
+// submitButton.type = "submit";
+// submitButton.innerText = "Отправить";
+// submitButton.disabled = true;
+// submitButton.onclick = () => {
+//   console.log(
+//     `Your data has been successfully sent: ${
+//       loginInput.value
+//     } ${passwordInput.getValue()}`
+//   );
+//   loginInput.value = "";
+//   passwordInput.setValue("");
+//   disabledSubmit();
+// };
+// disabledSubmit();
+// container.append(submitButton);
+
+// function disabledSubmit() {
+//   if (loginInput.value.length < 1 || passwordInput.getValue().length < 1) {
+//     submitButton.disabled = true;
+//   } else {
+//     submitButton.disabled = false;
+//   }
 // }
 
-// let p = new Password(document.body, true);
+// LOGIN FORM CONSTRUCTOR !!!
 
-// p.onChange = (data) => console.log(data); //буде корисно при виконаннi LoginForm та Password Verify
-// p.onOpenChange = (open) => console.log(open);
+// function LoginForm(parent, open) {
+//   const loginInput = document.createElement("input");
+//   loginInput.type = "text";
+//   loginInput.placeholder = "User Name";
+//   loginInput.oninput = () => {
+//     this.disabledButton();
+//   };
+//   parent.append(loginInput);
 
-// p.setValue("qwerty");
-// console.log(p.getValue());
+//   const passwordInput = new Password(parent, open);
+//   passwordInput.onChange = () => {
+//     this.disabledButton();
+//   };
 
-// p.setOpen(false);
-// console.log(p.getOpen());
+//   const submitButton = document.createElement("button");
+//   submitButton.type = "submit";
+//   submitButton.innerText = "Отправить";
+//   submitButton.onclick = () => {
+//     console.log(
+//       `Your data has been successfully sent: ${this.getLogin()} ${passwordInput.getValue()}`
+//     );
+//     this.setLogin("");
+//     passwordInput.setValue("");
+//     this.disabledButton();
+//   };
+//   parent.append(submitButton);
 
+//   this.getLogin = () => {
+//     return loginInput.value;
+//   };
+
+//   this.setLogin = (newLogin) => {
+//     loginInput.value = newLogin;
+//   };
+
+//   this.disabledButton = () => {
+//     if (this.getLogin().length < 1 || passwordInput.getValue().length < 1) {
+//       submitButton.disabled = true;
+//     } else {
+//       submitButton.disabled = false;
+//     }
+//   };
+//   this.disabledButton();
+// }
+
+// const container = document.createElement("div");
+// document.body.append(container);
+// const formNew = new LoginForm(container, false);
+
+// PASSWORD VERIFY !!! не сделал!!!!
